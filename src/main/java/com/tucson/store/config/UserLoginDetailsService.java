1d54a33bd1f5e680898a1a0eb6c36f993774871b
ginos1998 <ginosiam1998@gmail.com>
2023-03-25 04:22:23 -0300

    user login info is read from User class, which is connected to database. Not hardcoded anymore.

---
+++ src/main/java/com/tucson/store/config/UserLoginDetailsService.java
@@ -0,0 +1,26 @@
+package com.tucson.store.config;
+
+import com.tucson.store.entity.User;
+import com.tucson.store.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserLoginDetailsService implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+    Optional<User> user= userRepository.findByName(name);
+
+    return user.map(UserLoginUserDetails::new)
+                .orElseThrow(()->new UsernameNotFoundException("Usuario no encontrado :("));
+  }
+}
---
+++ src/main/java/com/tucson/store/config/UserLoginUserDetails.java
@@ -0,0 +1,61 @@
+package com.tucson.store.config;
+
+import com.tucson.store.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserLoginUserDetails implements UserDetails {
+
+  private String name;
+  private String password;
+  private List<GrantedAuthority> authorityList;
+
+  public UserLoginUserDetails(User user) {
+    name = user.getName();
+    password = user.getPassword();
+    authorityList= Arrays.stream(user.getRoles().split(","))
+                          .map(SimpleGrantedAuthority::new)
+                          .collect(Collectors.toList());
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorityList;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return name;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+}
