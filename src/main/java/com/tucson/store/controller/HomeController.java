523f4e0041e3ac74d693d4f0c6666d6e5b7644fa
ginos1998 <ginosiam1998@gmail.com>
2023-03-26 21:29:03 -0300

    both roles can access to index.html (main page after login)

--- src/main/java/com/tucson/store/controller/HomeController.java
+++ src/main/java/com/tucson/store/controller/HomeController.java
@@ -1,14 +1,10 @@
 package com.tucson.store.controller;
 
 import com.tucson.store.config.UserLoginUserDetails;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -17,10 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
   @GetMapping({"/", "/index"})
-  @PreAuthorize("hasAuthority('ROLE_USER')")
+  @PreAuthorize("hasAnyAuthority('ROLE_USER', 'ROLE_ADMIN')")
   public String indexPage(@AuthenticationPrincipal UserLoginUserDetails user) {
-    log.info("usuario logeado: " + user );
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     return "index";
   }
 }
